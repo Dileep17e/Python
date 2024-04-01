7b45ddc3aa7bd40ad616b8a0cd2991987c2377ea
sys <sys@user>
2024-04-01 21:34:29 +0530

    FIRST COMMIT

---
+++ Python/Condition_statements/if_ifelse_elif.py
@@ -0,0 +1,33 @@
+'''
+Created on Mar 28, 2024
+
+@author: sys
+'''
+
+
+age=int(input("please enter your age:"))
+'''
+if age>18:
+    print("you are an adult")
+elif age>=13:
+    print("you are a teenager")
+else:
+    print("you are a child")
+'''
+'''
+'''
+if age>13:
+    if age>18:
+       print("you are an adult")
+    else:
+       print("you are a teenager")
+       
+else:
+    print("you are a child")
+'''
+'''
+if age>18:
+    print("you are an adult")
+    
+         
+print("program stopped")
